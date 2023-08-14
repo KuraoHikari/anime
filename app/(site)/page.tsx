87e1361e3716576d81803f9a6ee65f5eaab7bace
KuraoHikari <dewaindra705@gmail.com>
2023-08-14 23:05:12 +0800

    update layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,18 @@
 import Jumbotron from "../components/Jumbotron";
+import SearchBar from "../components/SerachBar";
 
 export default function Home() {
-  return (
-    <div>
-      <Jumbotron />
-    </div>
-  );
+ return (
+  <div className="bg-gray-800 h-full">
+   <Jumbotron />
+   <div className="text-6xl text-center text-gray-200 my-5 font-extrabold">
+    <h1 className="hover:text-gray-500">
+     ANI<span className="text-gray-500 hover:text-gray-200">ME SE</span>ARCH
+    </h1>
+   </div>
+
+   <SearchBar />
+   <Jumbotron />
+  </div>
+ );
 }
---
+++ app/components/SerachBar.tsx
@@ -0,0 +1,17 @@
+const SearchBar = () => {
+ return (
+  <div className="mb-5 flex items-center justify-center">
+   <div className="flex w-[50rem] bg-white">
+    <input
+     type="search"
+     className="font-semibold w-full border-none bg-transparent px-4 py-1 text-gray-900 focus:outline-none"
+     placeholder="search"
+     x-model="search"
+    />
+    <button className="m-2 px-4 py-2 font-semibold text-gray-100 bg-gray-500">search</button>
+   </div>
+  </div>
+ );
+};
+
+export default SearchBar;
