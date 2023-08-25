2673d452c4e3744dcc0ed9786560283c27244506
KuraoHikari <dewaindra705@gmail.com>
2023-08-25 17:51:10 +0800

    minor update

--- app/anime-world/page.tsx
+++ app/anime-world/page.tsx
@@ -1,11 +1,13 @@
-import { AnimeCard } from "../components";
+import { AnimeCard, MegaTextTitle, SearchBar } from "../components";
 import DataAnimeExample from "../dataAnimeWEX";
 
 const AnimeWorld = () => {
  const animeDataExample = DataAnimeExample;
  return (
-  <div className="hidden lg:block ps-2 h-full">
-   <div className="md:mx-10 grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-5">
+  <div className="block ps-2 h-full">
+   <MegaTextTitle text1="THE" text2=" ANI" text3="ME SE" text4="ARCH" />
+   <SearchBar />
+   <div className="md:mx-10 mt-10 grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-5">
     {animeDataExample.map((anime) => (
      <AnimeCard
       key={anime?.mal_id}
